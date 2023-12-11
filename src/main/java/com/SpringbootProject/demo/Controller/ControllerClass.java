1b5515e23d044805861588f3d1e85f98a19b60f5
Tejaswi-1234 <153166540+Tejaswi-1234@users.noreply.github.com>
2023-12-11 10:14:36 +0000

    Update ControllerClass.java

--- src/main/java/com/SpringbootProject/demo/Controller/ControllerClass.java
+++ src/main/java/com/SpringbootProject/demo/Controller/ControllerClass.java
@@ -14,26 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.SpringbootProject.demo.Service.ServiceClass;
 
-@RestController
+@RestController //@Controller+@RequestBody
 public class ControllerClass {
-	@Autowired
+	@Autowired  //Automatic dependency injection
 	private ServiceClass serviceClass;
 	public ControllerClass(ServiceClass serviceClass) {
 		this.serviceClass=serviceClass;
 	}
-	@GetMapping("/stu")
+	@GetMapping("/stu") //handles get request(same like read operation in database)
 	public List<Map<String,Object>> getAllDetails(){
 		return serviceClass.getAllDetails();
 	}
-	@PostMapping("/stu1")
+	@PostMapping("/stu1") //used to insert the data into server
 	public String insertStu(@RequestBody Map<String,Object> student) {
 		return serviceClass.insertStu(student);
 	}
-	@PutMapping("/stu2/{id}")
+	@PutMapping("/stu2/{id}") //used to update or insert data
 	public String updateStuById(@PathVariable("id") int id,@RequestBody Map<String,Object> student1) {
 		return serviceClass.updateStuById(id,student1);
 	}
-	@DeleteMapping("/stu3/{id}")
+	@DeleteMapping("/stu3/{id}") //used to delete the data
 	public String deleteStuById(@PathVariable("id") int id) {
 		return serviceClass.deleteStuById(id);
 	}
